3b87eeafc30a05dedd78235b423702df7f647a47
Ervin Sejfulai <erse54105@edu.ucl.dk>
2024-11-24 19:18:34 +0100

    Plan Uge 2.1

--- src/components/WeekData.jsx
+++ src/components/WeekData.jsx
@@ -6,30 +6,61 @@ const weekData = [
         "I denne uge har jeg fokuseret på grundlæggende skitseteknikker for UI-design med en særlig vægt på rapid sketching. Gennem opgaverne har jeg eksperimenteret med at skabe forskellige layouts til en applikation, hvilket har inkluderet flere versioner af forsiden, navigationselementer og centrale UI-komponenter. Min tilgang har været at generere idéer hurtigt uden perfektionisme, hvilket har hjulpet mig med at udvikle en bred vifte af designmuligheder.",
         "Jeg har også læst i relevante kapitler fra Sketching User Experiences af Bill Buxton, specifikt kapitel 1-3, som omhandler værdien af skitsering i designprocessen. Dette har styrket min forståelse af, hvordan skitser kan fungere som et værktøj til hurtig udforskning og visualisering af koncepter.",
       ],
-      udfordringer: ["En væsentlig udfordring i ugens løb har været at tilpasse mig brugen af Figma som skitseværktøj. Brugen af Figma krævede lidt tilvænning, men jeg opdagede, at ved at kombinere enkle håndtegnede skitser, kunne jeg hurtigt eksperimentere med forskellige layouts.", "En anden udfordring var at undgå perfektionisme i mine skitser. Ifølge kapitlerne Sketches are not Prototypes og The Anatomy of Sketching​​, er skitser beregnet til at være hurtige og uden for mange detaljer for at fremme idéudvikling. Dette hjalp mig med at acceptere mine skitser som en del af en iterativ proces, hvor fejl og hurtige ændringer faktisk kan styrke det endelige design."],
-      refleksion: ["Ugens arbejde har styrket min forståelse af, hvordan hurtige skitser kan skabe en platform for idéudvikling uden at kræve en høj grad af præcision. Det har været lærerigt at se skitser som en kommunikationsform, hvor ambiguiteten kan inspirere nye idéer, som beskrevet i The Anatomy of Sketching. Min erfaring denne uge har også understreget værdien af at tage risici i de tidlige designfaser, som nævnt i “Fail early and fail often”-princippet​. Denne tilgang har givet mig friheden til at udforske forskellige koncepter uden at være bekymret for det endelige resultat."],
-      planForNaesteUge: ["Næste uge planlægger jeg at dykke dybere ned i sketching af UI-komponenter, især knapper, inputfelter og navigationsmenuer. Jeg vil fokusere på at eksperimentere med forskellige designvarianter for at udvikle en mere brugervenlig og visuelt tiltalende brugergrænseflade. Jeg vil også begynde at organisere mine skitser systematisk i Notion, så jeg lettere kan referere til dem og dele mine designforslag med mit team."],
-      loesteOpgaver: ["Skitsér forskellige layouts til din applikation, herunder flere versioner af forsiden, navigationselementer og centrale UI-komponenter. Fokuser på at generere mange idéer hurtigt uden at være perfektionistisk."],
+      udfordringer: [
+        "En væsentlig udfordring i ugens løb har været at tilpasse mig brugen af Figma som skitseværktøj. Brugen af Figma krævede lidt tilvænning, men jeg opdagede, at ved at kombinere enkle håndtegnede skitser, kunne jeg hurtigt eksperimentere med forskellige layouts.",
+        "En anden udfordring var at undgå perfektionisme i mine skitser. Ifølge kapitlerne Sketches are not Prototypes og The Anatomy of Sketching​​, er skitser beregnet til at være hurtige og uden for mange detaljer for at fremme idéudvikling. Dette hjalp mig med at acceptere mine skitser som en del af en iterativ proces, hvor fejl og hurtige ændringer faktisk kan styrke det endelige design.",
+      ],
+      refleksion: [
+        "Ugens arbejde har styrket min forståelse af, hvordan hurtige skitser kan skabe en platform for idéudvikling uden at kræve en høj grad af præcision. Det har været lærerigt at se skitser som en kommunikationsform, hvor ambiguiteten kan inspirere nye idéer, som beskrevet i The Anatomy of Sketching. Min erfaring denne uge har også understreget værdien af at tage risici i de tidlige designfaser, som nævnt i “Fail early and fail often”-princippet​. Denne tilgang har givet mig friheden til at udforske forskellige koncepter uden at være bekymret for det endelige resultat.",
+      ],
+      planForNaesteUge: [
+        "Næste uge planlægger jeg at dykke dybere ned i sketching af UI-komponenter, især knapper, inputfelter og navigationsmenuer. Jeg vil fokusere på at eksperimentere med forskellige designvarianter for at udvikle en mere brugervenlig og visuelt tiltalende brugergrænseflade. Jeg vil også begynde at organisere mine skitser systematisk i Notion, så jeg lettere kan referere til dem og dele mine designforslag med mit team.",
+      ],
+      loesteOpgaver: [
+        "Skitsér forskellige layouts til din applikation, herunder flere versioner af forsiden, navigationselementer og centrale UI-komponenter. Fokuser på at generere mange idéer hurtigt uden at være perfektionistisk.",
+      ],
     },
     rightSide: {
-      viden: ["Ved at skabe flere versioner af forsiden, navigationselementer og centrale UI-komponenter har jeg fået en grundlæggende forståelse for skitseteknikker i UI-design og set værdien af skitsering som en integreret del af designprocessen. Denne proces har givet mig kendskab til forskellige typer UI-komponenter som knapper, inputfelter og navigationsmenuer og deres funktion i brugergrænsefladen. Ved at fokusere på hurtig skitsering uden perfektionisme har jeg opnået viden om, hvordan skitsering kan bruges til at udforske og udvikle idéer effektivt."],
-      faerdigheder: ["I arbejdet med hurtigt at generere flere skitser har jeg udviklet mine færdigheder i at skitsere forskellige UI-layouter og navigationsstrukturer, hvilket har gjort det muligt for mig at evaluere og kritisk vurdere dem. Ved at variere designet af centrale UI-komponenter har jeg eksperimenteret med forskellige tilgange for at identificere de mest brugervenlige og æstetiske løsninger. Jeg har anvendt digitale værktøjer som Figma til at oprette og organisere mine skitser, hvilket har givet mig mulighed for at gemme dem til senere brug og feedback."],
-      kompetencer: ["Gennem ugens opgaver har jeg brugt skitsering som et værktøj til at visualisere mange idéer hurtigt, hvilket har forbedret min effektivitet i den tidlige designfase. Jeg har etableret en struktur for mine skitser og layouts, hvilket gør dem let tilgængelige og nemme at dele med andre via dokumentationsværktøjer som Notion og Google Docs. Dette har gjort det muligt for mig at tilpasse og justere mine skitser løbende baseret på feedback og mine egne designobservationer. Dette arbejde har styrket min brugerdrevne tilgang i designprocessen og sikret, at jeg holder fokus på brugerens behov og oplevelse."],
+      viden: [
+        "Ved at skabe flere versioner af forsiden, navigationselementer og centrale UI-komponenter har jeg fået en grundlæggende forståelse for skitseteknikker i UI-design og set værdien af skitsering som en integreret del af designprocessen. Denne proces har givet mig kendskab til forskellige typer UI-komponenter som knapper, inputfelter og navigationsmenuer og deres funktion i brugergrænsefladen. Ved at fokusere på hurtig skitsering uden perfektionisme har jeg opnået viden om, hvordan skitsering kan bruges til at udforske og udvikle idéer effektivt.",
+      ],
+      faerdigheder: [
+        "I arbejdet med hurtigt at generere flere skitser har jeg udviklet mine færdigheder i at skitsere forskellige UI-layouter og navigationsstrukturer, hvilket har gjort det muligt for mig at evaluere og kritisk vurdere dem. Ved at variere designet af centrale UI-komponenter har jeg eksperimenteret med forskellige tilgange for at identificere de mest brugervenlige og æstetiske løsninger. Jeg har anvendt digitale værktøjer som Figma til at oprette og organisere mine skitser, hvilket har givet mig mulighed for at gemme dem til senere brug og feedback.",
+      ],
+      kompetencer: [
+        "Gennem ugens opgaver har jeg brugt skitsering som et værktøj til at visualisere mange idéer hurtigt, hvilket har forbedret min effektivitet i den tidlige designfase. Jeg har etableret en struktur for mine skitser og layouts, hvilket gør dem let tilgængelige og nemme at dele med andre via dokumentationsværktøjer som Notion og Google Docs. Dette har gjort det muligt for mig at tilpasse og justere mine skitser løbende baseret på feedback og mine egne designobservationer. Dette arbejde har styrket min brugerdrevne tilgang i designprocessen og sikret, at jeg holder fokus på brugerens behov og oplevelse.",
+      ],
     },
   },
   {
     week: 34,
     leftSide: {
-      oversigt: ["I denne uge har jeg primært arbejdet på at udvikle og visualisere User Flows for de centrale interaktioner i mit projekt, såsom registrering, login, booking og checkout-processen. Ved hjælp af værktøjer som Figma og Miro har jeg struktureret og illustreret de vigtigste brugerrejser i applikationen, hvilket har givet et klart billede af, hvordan brugerne forventes at navigere. Desuden har jeg opdelt de mere komplekse flows i sektioner, hvilket har hjulpet med at organisere de forskellige trin og interaktioner i projektet."],
-      udfordringer: ["En udfordring var at sikre, at alle mulige interaktioner og forgreninger i User Flows blev korrekt indarbejdet, især for de komplekse dele som booking og checkout. Materialer som The User Experience Team of One og artiklen fra NNGroup gav værdifuld vejledning i, hvordan man strukturerer og dokumenterer User Flows effektivt."],
-      refleksion: ["Denne uge har styrket min forståelse af, hvordan User Flows kan forbedre brugeroplevelsen ved at synliggøre og optimere navigationsvejene. Gennem arbejdet med User Flows har jeg opdaget, hvor vigtigt det er at have en struktureret tilgang, der sikrer en logisk og brugervenlig opbygning af applikationen. At opdele komplekse flows i sektioner har gjort det nemmere at få overblik over brugerrejsen og identificere specifikke områder, hvor brugeroplevelsen kan forbedres."],
-      planForNaesteUge: ["Næste uge planlægger jeg at fokusere på at udvikle mere detaljerede wireframes og begynde at integrere dem i prototyper. Jeg vil fortsætte med at bruge User Flows som grundlag for den interaktive funktionalitet og arbejde på at iterere over designet baseret på feedback. Målet er at skabe en sammenhængende oplevelse, der sikrer, at navigationen gennem applikationen er intuitiv og problemfri."],
-      loesteOpgaver: ["Lav User Flows for de vigtigste interaktioner i dit projekt. Opret f.eks. flows for registrering, login, booking, og checkout-processen. Sørg for at inkludere alle potentielle interaktionstrin."],
+      oversigt: [
+        "I denne uge har jeg primært arbejdet på at udvikle og visualisere User Flows for de centrale interaktioner i mit projekt, såsom registrering, login, booking og checkout-processen. Ved hjælp af værktøjer som Figma og Miro har jeg struktureret og illustreret de vigtigste brugerrejser i applikationen, hvilket har givet et klart billede af, hvordan brugerne forventes at navigere. Desuden har jeg opdelt de mere komplekse flows i sektioner, hvilket har hjulpet med at organisere de forskellige trin og interaktioner i projektet.",
+      ],
+      udfordringer: [
+        "En udfordring var at sikre, at alle mulige interaktioner og forgreninger i User Flows blev korrekt indarbejdet, især for de komplekse dele som booking og checkout. Materialer som The User Experience Team of One og artiklen fra NNGroup gav værdifuld vejledning i, hvordan man strukturerer og dokumenterer User Flows effektivt.",
+      ],
+      refleksion: [
+        "Denne uge har styrket min forståelse af, hvordan User Flows kan forbedre brugeroplevelsen ved at synliggøre og optimere navigationsvejene. Gennem arbejdet med User Flows har jeg opdaget, hvor vigtigt det er at have en struktureret tilgang, der sikrer en logisk og brugervenlig opbygning af applikationen. At opdele komplekse flows i sektioner har gjort det nemmere at få overblik over brugerrejsen og identificere specifikke områder, hvor brugeroplevelsen kan forbedres.",
+      ],
+      planForNaesteUge: [
+        "Næste uge planlægger jeg at fokusere på at udvikle mere detaljerede wireframes og begynde at integrere dem i prototyper. Jeg vil fortsætte med at bruge User Flows som grundlag for den interaktive funktionalitet og arbejde på at iterere over designet baseret på feedback. Målet er at skabe en sammenhængende oplevelse, der sikrer, at navigationen gennem applikationen er intuitiv og problemfri.",
+      ],
+      loesteOpgaver: [
+        "Lav User Flows for de vigtigste interaktioner i dit projekt. Opret f.eks. flows for registrering, login, booking, og checkout-processen. Sørg for at inkludere alle potentielle interaktionstrin.",
+      ],
     },
     rightSide: {
-      viden: ["", ""],
-      faerdigheder: ["", ""],
-      kompetencer: [""],
+      viden: [
+        "Gennem arbejdet med User Flows har jeg opnået en dybere forståelse for, hvordan brugerrejser kan visualiseres for at optimere applikationens struktur og navigation, hvilket stemmer overens med målet om at forstå betydningen af User Flows i UX-design. Ved at opdele komplekse flows i logiske sektioner, som f.eks. registrerings- og checkout-processerne, har jeg også kunnet sikre en mere struktureret forståelse af brugerinteraktionerne, der gør det lettere at navigere og planlægge brugeroplevelsen.",
+      ],
+      faerdigheder: [
+        "I forhold til færdigheder har jeg arbejdet målrettet med Figma og Miro for at skabe og dokumentere User Flows, hvilket har givet mig praktisk erfaring med disse værktøjer til at organisere og gemme flows til senere brug. Disse opgaver har hjulpet mig med at sikre, at alle nødvendige interaktionstrin er inkluderet i de centrale brugerrejser, hvilket styrker mine visuelle og tekniske færdigheder inden for dokumentation.",
+      ],
+      kompetencer: [
+        "Endelig har jeg opnået kompetencerne til at kritisk analysere og strukturere brugerflows, så de understøtter en mere intuitiv brugeroplevelse. Denne uges opgaver har givet mig en mere detaljeret forståelse af, hvordan man kan anvende brugercentrerede designmetoder i dokumentation og planlægning, og dermed sikre, at brugerens behov forbliver i fokus i hele udviklingsprocessen.",
+      ],
     },
   },
   {
